b857a335ed105287fcb776589f6f86335346f341
M.Persada <ersad40@gmail.com>
2015-12-18 03:29:54 -0500

    first commit

---
+++ app/api/api.rb
@@ -0,0 +1,12 @@
+require 'v1/bycyclev1.rb'
+
+
+module API
+
+  class Base < Grape::API
+
+    mount API::BycycleV1
+
+  end
+
+end
---
+++ app/api/v1/bycyclev1.rb
@@ -0,0 +1,79 @@
+module API
+
+  class BycycleV1 < Grape::API
+
+    prefix "v1"
+
+    format :json
+
+
+
+    #http_basic do |username, password|
+
+     # user = User.where(username: username).first
+
+      #user.authorized? password unless user.blank?
+
+    #end
+
+
+
+    resource :bycycles do
+
+      desc "Get all bycycles"
+
+      get '/' do
+
+        @bycycles = Bycycle.all
+
+        {bycycles: @bycycles, total: @bycycles.count, status: 200}
+
+      end
+
+
+
+      desc "Add new bycycle"
+
+      post '/' do
+
+        Bycycle.create!({name: params[:name], series: params[:series]})
+
+      end
+
+
+
+      desc "Show a bycycle"
+
+      get '/:id' do
+
+        @bycycle = Bycycle.find_by_id params[:id]
+
+        {bycycles: @bycycle, status: 200}
+
+      end
+
+
+
+      desc "Update a bycycle"
+
+      put '/:id' do
+
+  Bycycle.find_by_id(params[:id]).update({name: params[:name], series: params[:series]})
+
+      end
+
+
+
+      desc "Delete a data"
+
+      delete '/:id' do
+
+        Bycycle.find_by_id(params[:id]).destroy
+
+      end
+
+    end
+
+  end
+
+end
